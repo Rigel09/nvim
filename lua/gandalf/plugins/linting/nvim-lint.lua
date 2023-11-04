ea40793b019a15079e6fc4431b82b8c1f6ed4282
Joshua Newton <joshua.newton102@gmail.com>
2023-11-04 10:45:42 -0500

    "Better linting. Added bash lang server support"

--- lua/gandalf/plugins/linting/nvim-lint.lua
+++ lua/gandalf/plugins/linting/nvim-lint.lua
@@ -1,27 +1,27 @@
-local M =
-{
-  "mfussenegger/nvim-lint",
+local M = {
+  'mfussenegger/nvim-lint',
   lazy = true,
-  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+  event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
   config = function()
-    local lint = require("lint")
+    local lint = require 'lint'
 
     lint.linters_by_ft = {
-      python = { "pylint" },
+      python = { 'pylint', 'mypy' },
+      sh = { 'shellcheck' },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
     })
 
-    vim.keymap.set("n", "<leader>ll", function()
+    vim.keymap.set('n', '<leader>ll', function()
       lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
+    end, { desc = 'Trigger linting for current file' })
   end,
 }
 
--- lua/gandalf/plugins/no-config.lua
+++ lua/gandalf/plugins/no-config.lua
@@ -1,24 +1,22 @@
 local M = {
-  
+
   --- Git related plugins
-  "tpope/vim-fugitive", --- Git plugin
-  "tpope/vim-rhubarb", --- Allows you to open git URLS?
+  'tpope/vim-fugitive', --- Git plugin
+  'tpope/vim-rhubarb', --- Allows you to open git URLS?
 
   --- Detect tabstop and shiftwidth automatically
   --- Changes tab width based on the file being modified
   'tpope/vim-sleuth',
 
-
   -- ***************************************
   -- Editor
   -- ***************************************
-  { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "MunifTanjim/nui.nvim", lazy = true },
-
+  { 'nvim-tree/nvim-web-devicons', lazy = true },
+  { 'MunifTanjim/nui.nvim', lazy = true },
 
   -- ***************************************
   -- Misc
   -- ***************************************
-  { "nvim-lua/plenary.nvim", lazy = true }
+  { 'nvim-lua/plenary.nvim', lazy = true },
 }
 return M
