9032786940d315d154bc621cccd381b68adb6fa6
Rigel09 <joshua.newton102@gmail.com>
2024-01-14 09:58:19 -0600

    Added borders to lsp docs

--- lua/gandalf/plugins/lsp/nvim-lspconfig.lua
+++ lua/gandalf/plugins/lsp/nvim-lspconfig.lua
@@ -1,3 +1,72 @@
+local my_attach_func = function(client, bufnr)
+  local keymap = vim.keymap -- for conciseness
+  local opts = { noremap = true, silent = true }
+
+  opts.buffer = bufnr
+
+  -- set keybinds
+  opts.desc = 'Show LSP references'
+  keymap.set('n', 'gR', '<cmd>Telescope lsp_references<CR>', opts) -- show definition, references
+
+  opts.desc = 'Go to declaration'
+  keymap.set('n', 'gD', vim.lsp.buf.declaration, opts) -- go to declaration
+
+  opts.desc = 'Show LSP definitions'
+  keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts) -- show lsp definitions
+
+  opts.desc = 'Show LSP implementations'
+  keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) -- show lsp implementations
+
+  opts.desc = 'Show LSP type definitions'
+  keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<CR>', opts) -- show lsp type definitions
+
+  opts.desc = 'See available code actions'
+  keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+  opts.desc = 'Smart rename'
+  keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- smart rename
+
+  opts.desc = 'Show buffer diagnostics'
+  keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', opts) -- show  diagnostics for file
+
+  opts.desc = 'Show line diagnostics'
+  keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+  opts.desc = 'Go to previous diagnostic'
+  keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+  opts.desc = 'Go to next diagnostic'
+  keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+  opts.desc = 'Show documentation for what is under cursor'
+  keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+  opts.desc = 'Show signature for what is under cursor'
+  keymap.set('n', 'S', vim.lsp.buf.signature_help, opts) -- show signature for what is under cursor
+
+  opts.desc = 'Restart LSP'
+  keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts) -- mapping to restart lsp if necessary
+end
+
+local border = {
+  { '🭽', 'FloatBorder' },
+  { '▔', 'FloatBorder' },
+  { '🭾', 'FloatBorder' },
+  { '▕', 'FloatBorder' },
+  { '🭿', 'FloatBorder' },
+  { '▁', 'FloatBorder' },
+  { '🭼', 'FloatBorder' },
+  { '▏', 'FloatBorder' },
+}
+
+-- LSP settings (for overriding per client)
+local handlers = {
+  ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+  ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+}
+
+-- ================================================================================================
+-- ================================================================================================
 local M = {
   'neovim/nvim-lspconfig',
   event = { 'BufReadPre', 'BufNewFile' },
@@ -8,74 +77,23 @@ local M = {
     'folke/neodev.nvim',
   },
   config = function()
-    local function has(plugin)
-      return require('lazy.core.config').spec.plugins[plugin] ~= nil
-    end
-
-    if has 'neoconf.nvim' then
-      local plugin = require('lazy.core.config').spec.plugins['neoconf.nvim']
-      require('neoconf').setup(require('lazy.core.plugin').values(plugin, 'opts', false))
-    end
     require('neoconf').setup {}
-
     require('neodev').setup()
 
+    -- ********************************************************************************************
     -- import lspconfig plugin
+    -- ********************************************************************************************
+
+    -- This is for adding borders around popup docs
+    vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+    vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
     local lspconfig = require 'lspconfig'
 
+    -- ********************************************************************************************
     -- import cmp-nvim-lsp plugin
+    -- ********************************************************************************************
     local cmp_nvim_lsp = require 'cmp_nvim_lsp'
-
-    local keymap = vim.keymap -- for conciseness
-
-    local opts = { noremap = true, silent = true }
-    local on_attach = function(client, bufnr)
-      opts.buffer = bufnr
-      -- client.resolved_capabilities.hover = true
-
-      -- set keybinds
-      opts.desc = 'Show LSP references'
-      keymap.set('n', 'gR', '<cmd>Telescope lsp_references<CR>', opts) -- show definition, references
-
-      opts.desc = 'Go to declaration'
-      keymap.set('n', 'gD', vim.lsp.buf.declaration, opts) -- go to declaration
-
-      opts.desc = 'Show LSP definitions'
-      keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts) -- show lsp definitions
-
-      opts.desc = 'Show LSP implementations'
-      keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) -- show lsp implementations
-
-      opts.desc = 'Show LSP type definitions'
-      keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<CR>', opts) -- show lsp type definitions
-
-      opts.desc = 'See available code actions'
-      keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      opts.desc = 'Smart rename'
-      keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- smart rename
-
-      opts.desc = 'Show buffer diagnostics'
-      keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', opts) -- show  diagnostics for file
-
-      opts.desc = 'Show line diagnostics'
-      keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = 'Go to previous diagnostic'
-      keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = 'Go to next diagnostic'
-      keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = 'Show documentation for what is under cursor'
-      keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = 'Show signature for what is under cursor'
-      keymap.set('n', 'S', vim.lsp.buf.signature_help, opts) -- show signature for what is under cursor
-
-      opts.desc = 'Restart LSP'
-      keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts) -- mapping to restart lsp if necessary
-    end
 
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -89,54 +107,40 @@ local M = {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
     end
 
-    -- configure html server
-    lspconfig['html'].setup {
+    -- ********************************************************************************************
+    -- Server Setup
+    -- ********************************************************************************************
+
+    -- -------------------------------------
+    -- BASH
+    -- -------------------------------------
+    lspconfig['bashls'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
+      handlers = handlers,
+      on_attach = my_attach_func,
+      filetypes = { 'tmux' },
     }
-    -- configure python server
-    lspconfig['pylsp'].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-    }
+
+    -- -------------------------------------
+    -- C++
+    -- -------------------------------------
     lspconfig['clangd'].setup {
       capabilities = capabilities,
+      handlers = handlers,
       on_attach = function(client, bufnr)
-        on_attach(client, bufnr)
+        my_attach_func(client, bufnr)
         require('clangd_extensions.inlay_hints').setup_autocmd()
         require('clangd_extensions.inlay_hints').set_inlay_hints()
       end,
     }
-    lspconfig['bashls'].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { 'tmux' },
-    }
-    lspconfig['matlab_ls'].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-    }
-    -- configure rust server
-    lspconfig['rust_analyzer'].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { 'rust' },
-      root_dir = require('lspconfig.util').root_pattern 'Cargo.toml',
-      settings = {
-        ['rust_analyzer'] = {
-          checkOnSave = {
-            enable = true,
-            command = 'clippy',
-          },
-          cargo = { allFeatures = true },
-        },
-      },
-    }
 
-    -- configure lua server (with special settings)
+    -- -------------------------------------
+    -- Lua
+    -- -------------------------------------
     lspconfig['lua_ls'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
+      on_attach = my_attach_func,
+      handlers = handlers,
       settings = { -- custom settings for lua
         Lua = {
           -- make the language server recognize "vim" global
@@ -160,6 +164,44 @@ local M = {
           },
         },
       },
+    }
+
+    -- -------------------------------------
+    -- MATLAB
+    -- -------------------------------------
+    lspconfig['matlab_ls'].setup {
+      capabilities = capabilities,
+      handlers = handlers,
+      on_attach = my_attach_func,
+    }
+
+    -- -------------------------------------
+    -- Rust
+    -- -------------------------------------
+    lspconfig['rust_analyzer'].setup {
+      capabilities = capabilities,
+      handlers = handlers,
+      on_attach = my_attach_func,
+      filetypes = { 'rust' },
+      root_dir = require('lspconfig.util').root_pattern 'Cargo.toml',
+      settings = {
+        ['rust_analyzer'] = {
+          checkOnSave = {
+            enable = true,
+            command = 'clippy',
+          },
+          cargo = { allFeatures = true },
+        },
+      },
+    }
+
+    -- -------------------------------------
+    -- Python
+    -- -------------------------------------
+    lspconfig['pylsp'].setup {
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = my_attach_func,
     }
   end,
 }
