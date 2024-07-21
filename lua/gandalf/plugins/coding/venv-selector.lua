926627840127fd3f8566a8b29317140986165a72
Rigel09 <joshua.newton102@gmail.com>
2024-07-20 21:21:22 -0500

    Updated venv-select to new rewrite branch

--- lua/gandalf/plugins/coding/venv-selector.lua
+++ lua/gandalf/plugins/coding/venv-selector.lua
@@ -7,15 +7,13 @@ local M = {
     -- auto_refresh = false
     fd_binary_name = 'fdfind',
   },
-  -- config = true,
-  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  lazy = false,
+  branch = 'regexp',
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
     { '<leader>vs', '<cmd>:VenvSelect<cr>', { desc = 'Select python venv' } },
-
-    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>:VenvSelectCached<cr>', { desc = 'Grab prev python env' } },
   },
+  config = true,
 }
 
 return M
