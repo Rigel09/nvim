ac676ce19edc273228227b305a3abf82305fad3c
Rigel09 <joshua.newton102@gmail.com>
2024-07-20 20:55:07 -0500

    Updated plugins updated scope plugins - Removed mini-indent-scope - Updated config for indent-blankline

--- lua/gandalf/plugins/editor/indent-blankline.lua
+++ lua/gandalf/plugins/editor/indent-blankline.lua
@@ -1,30 +1,71 @@
 local M = {
   'lukas-reineke/indent-blankline.nvim',
   event = 'VeryLazy',
-  opts = {
-    indent = {
-      char = '│',
-      tab_char = '│',
-    },
-    scope = { enabled = false },
-    exclude = {
-      filetypes = {
-        'help',
-        'alpha',
-        'dashboard',
-        'neo-tree',
-        'Trouble',
-        'trouble',
-        'lazy',
-        'mason',
-        'notify',
-        'toggleterm',
-        'lazyterm',
-        'dashboard',
+  config = function()
+    local indent_highlight = {
+      'CursorColumn',
+      'Whitespace',
+    }
+
+    local scope_highlight = {
+      'RainbowRed',
+      'RainbowYellow',
+      'RainbowBlue',
+      'RainbowOrange',
+      'RainbowGreen',
+      'RainbowViolet',
+      'RainbowCyan',
+    }
+    local hooks = require 'ibl.hooks'
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+      vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+      vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+      vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+      vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+      vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+      vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+    end)
+
+    vim.g.rainbow_delimiters = { highlight = scope_highlight }
+    require('ibl').setup {
+      indent = {
+        -- char = '',
+        -- tab_char = '|',
+        -- highlight = indent_highlight,
       },
-    },
-  },
-  main = 'ibl',
+
+      whitespace = {
+        highlight = indent_highlight,
+        remove_blankline_trail = false,
+      },
+
+      scope = {
+        highlight = scope_highlight,
+      },
+
+      exclude = {
+        filetypes = {
+          'help',
+          'alpha',
+          'dashboard',
+          'neo-tree',
+          'Trouble',
+          'trouble',
+          'lazy',
+          'mason',
+          'notify',
+          'toggleterm',
+          'lazyterm',
+          'dashboard',
+        },
+      },
+    }
+
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+  end,
 }
 
 return M
