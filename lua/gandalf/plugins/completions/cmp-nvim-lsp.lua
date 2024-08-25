8943ce0b40eff032962eefae17d5e553c89ca8a2
Rigel09 <joshua.newton102@gmail.com>
2024-08-25 01:43:29 -0500

    Brought back lua snip

--- lua/gandalf/plugins/completions/cmp-nvim-lsp.lua
+++ lua/gandalf/plugins/completions/cmp-nvim-lsp.lua
@@ -5,17 +5,17 @@ local M = {
     'hrsh7th/cmp-buffer', -- source for text in buffer
     'hrsh7th/cmp-path', -- source for file system paths
     'hrsh7th/cmp-nvim-lsp-signature-help', -- source for file system paths
-    -- 'L3MON4D3/LuaSnip', -- snippet engine
+    'L3MON4D3/LuaSnip', -- snippet engine
     'saadparwaiz1/cmp_luasnip', -- for autocompletion
     'onsails/lspkind.nvim', -- vs-code like pictograms
   },
   config = function()
     local cmp = require 'cmp'
-    -- local luasnip = require 'luasnip'
+    local luasnip = require 'luasnip'
     local lspkind = require 'lspkind'
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    -- require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load()
 
     cmp.setup {
       window = {
@@ -32,11 +32,11 @@ local M = {
       completion = {
         completeopt = 'menu,menuone,preview,noselect',
       },
-      -- snippet = { -- configure how nvim-cmp interacts with snippet engine
-      --   expand = function(args)
-      --     luasnip.lsp_expand(args.body)
-      --   end,
-      -- },
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
       mapping = cmp.mapping.preset.insert {
         ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
         ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
@@ -49,7 +49,7 @@ local M = {
       -- sources for autocompletion
       sources = cmp.config.sources {
         { name = 'nvim_lsp' },
-        -- { name = 'luasnip' }, -- snippets
+        { name = 'luasnip' }, -- snippets
         { name = 'buffer' }, -- text within current buffer
         { name = 'path' }, -- file system paths
         { name = 'nvim_lsp_signature_help' },
