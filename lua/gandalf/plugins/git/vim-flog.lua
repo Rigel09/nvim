cf73efb57da90dbe156f0689d4980e8a53964218
Rigel09 <joshua.newton102@gmail.com>
2024-09-07 18:06:07 -0500

    Added keybind for Flog ordered by date

--- lua/gandalf/plugins/git/vim-flog.lua
+++ lua/gandalf/plugins/git/vim-flog.lua
@@ -2,6 +2,9 @@ local M = {
   'rbong/vim-flog',
   lazy = true,
   cmd = { 'Flog', 'Flogsplit', 'Floggit' },
+  keys = {
+    { '<leader>fg', '<cmd>Flog -order=date<CR>', desc = 'Show Flog git graph ordered by date' },
+  },
   dependencies = {
     'tpope/vim-fugitive',
   },
