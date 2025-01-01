2d463930938733c1f7e582e26471a73c6b14c984
Rigel09 <joshua.newton102@gmail.com>
2024-12-31 23:26:19 -0600

    Added timber to config

---
+++ lua/gandalf/plugins/coding/timber.lua
@@ -0,0 +1,34 @@
+local M = {
+  'Goose97/timber.nvim',
+  version = '*', -- Use for stabilty; omit to use `main` branch for the latest features
+  event = 'VeryLazy',
+  keys = {
+    {
+      '<leader>glc',
+      function()
+        require('timber.actions').clear_log_statements { global = false }
+      end,
+      desc = 'Find Buffers',
+    },
+    {
+      '<leader>glt',
+      function()
+        require('timber.actions').toggle_comment_log_statements { global = false }
+      end,
+      desc = 'Find Buffers',
+    },
+  },
+  config = function()
+    require('timber').setup {
+      log_templates = {
+        default = {
+          python = [[print(f"%log_marker %filename:%line_number - %log_target: {%log_target}")]],
+          cpp = [[std::cout << "%log_marker %filename:%line_number - %log_target: " << %log_target << std::endl;]],
+        },
+      },
+      log_marker = 'JLOG',
+    }
+  end,
+}
+
+return M
