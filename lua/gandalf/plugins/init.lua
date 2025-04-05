4a11573563d86a0bce413969eb6150ce7e672e33
Rigel09 <joshua.newton102@gmail.com>
2025-04-04 23:46:58 -0500

    moved telescope to editor folder

--- lua/gandalf/plugins/init.lua
+++ lua/gandalf/plugins/init.lua
@@ -3,7 +3,6 @@ local M = {
   { import = 'gandalf.plugins.ui' },
   { import = 'gandalf.plugins.editor' },
   { import = 'gandalf.plugins.tree-sitter' },
-  { import = 'gandalf.plugins.telescope' },
   { import = 'gandalf.plugins.lsp' },
   { import = 'gandalf.plugins.linting' },
   { import = 'gandalf.plugins.formatting' },
