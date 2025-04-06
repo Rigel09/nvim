1afbdf9a6ad85c9b411311514d7226862b23ec20
Rigel09 <joshua.newton102@gmail.com>
2025-04-06 10:31:58 -0500

    use more default keymaps

--- lua/gandalf/plugins/linting/nvim-lint.lua
+++ lua/gandalf/plugins/linting/nvim-lint.lua
@@ -21,10 +21,6 @@ local M = {
         lint.try_lint()
       end,
     })
-
-    vim.keymap.set('n', '<leader>ll', function()
-      lint.try_lint()
-    end, { desc = 'Trigger linting for current file' })
   end,
 }
 
