7d922768fa2cee65c3d4e25615feadddfed371a6
Rigel09 <joshua.newton102@gmail.com>
2025-04-06 09:32:24 -0500

    Remove apps and recent files from dashboard, get rid of useless shortcuts

--- lua/gandalf/plugins/editor/nvim-dashboard.lua
+++ lua/gandalf/plugins/editor/nvim-dashboard.lua
@@ -9,28 +9,17 @@ local M = {
           enable = true,
         },
         shortcut = {
-          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
           {
             icon = ' ',
             icon_hl = '@variable',
             desc = 'Files',
             group = 'Label',
-            action = 'Telescope find_files',
+            action = 'FzfLua find_files',
             key = 'f',
           },
-          {
-            desc = ' Apps',
-            group = 'DiagnosticHint',
-            action = 'Telescope app',
-            key = 'a',
-          },
-          {
-            desc = ' dotfiles',
-            group = 'Number',
-            action = 'Telescope dotfiles',
-            key = 'd',
-          },
         },
+        project = { enable = false },
+        mru = { enable = false },
       },
     }
   end,
