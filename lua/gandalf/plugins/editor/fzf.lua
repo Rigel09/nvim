7c938ba5d8e282896f69506e37a51c5e68fb8b49
Rigel09 <joshua.newton102@gmail.com>
2025-04-06 09:31:52 -0500

    Removed unused fzf keymaps

--- lua/gandalf/plugins/editor/fzf.lua
+++ lua/gandalf/plugins/editor/fzf.lua
@@ -10,7 +10,6 @@ local M = {
     { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Find Files (root dir)' },
     { '<leader>fo', '<cmd>FzfLua oldfiles<cr>', desc = 'Find Old Files' },
     { '<leader>fR', '<cmd>FzfLua resume<cr>', desc = 'Resume prev search' },
-    { '<leader>ft', '<cmd>FzfLua tabs<cr>', desc = 'Find Tabs' },
     { '<leader>fw', '<cmd>FzfLua grep_cword<cr>', desc = 'Find Word Under Cursor' },
     { '<leader>fW', '<cmd>FzfLua grep_cWORD<cr>', desc = 'Find WORD Under Cursor' },
     { '<leader>lg', '<cmd>FzfLua live_grep<cr>', desc = 'Live grep project' },
@@ -20,14 +19,11 @@ local M = {
     -- LSP
     -- ------------------------------------------------------------
     { '<leader>fr', '<cmd>FzfLua lsp_references<cr>', desc = 'Find LSP Reference' },
-    { '<leader>ws', '<cmd>FzfLua lsp_workspace_symbols<cr>', desc = 'Find Workspace Symbols' },
-    { '<leader>ds', '<cmd>FzfLua lsp_document_symbols<cr>', desc = 'Find Documnet Symbols' },
 
     -- ------------------------------------------------------------
     -- Misc
     -- ------------------------------------------------------------
     { '<leader>ss', '<cmd>FzfLua spell_suggest<cr>', desc = 'Spelling Suggestions' },
-    { '<leader>ch', '<cmd>FzfLua command_history<cr>', desc = 'Command History' },
   },
   config = function()
     -- calling `setup` is optional for customization
