17e40127a394ba847f60f13437ad2930f0dee104
Rigel09 <joshua.newton102@gmail.com>
2025-04-08 21:54:18 -0500

    delete lazygit

--- lua/gandalf/plugins/snacks.lua
+++ lua/gandalf/plugins/snacks.lua
@@ -2,24 +2,9 @@ local M = {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
-  -- opts = {
-  --   -- your configuration comes here
-  --   -- or leave it empty to use the default settings
-  --   -- refer to the configuration section below
-  --   bigfile = { enabled = true },
-  --   dashboard = { enabled = true },
-  --   indent = { enabled = true },
-  --   input = { enabled = true },
-  --   lazygit = { enabled = true },
-  --   notifier = { enabled = true },
-  --   picker = { enabled = true },
-  --   rename = { enabled = true },
-  --   quickfile = { enabled = true },
-  --   scope = { enabled = true },
-  --   scroll = { enabled = true },
-  --   statuscolumn = { enabled = true },
-  --   words = { enabled = true },
-  -- },
+  keys = {
+    { '<leader>lt', '<cmd>lua require("snacks").lazygit.open()<cr>', desc = 'LazyGit' },
+  },
   config = function()
     local snacks = require 'snacks'
     snacks.setup {
